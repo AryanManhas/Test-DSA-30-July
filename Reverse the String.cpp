35af21607a06424a15a9a56f376e5be84156913c
AryanManhas <122689335+AryanManhas@users.noreply.github.com>
2024-07-30 14:34:26 +0530

    Reverse the String.cpp

---
+++ Reverse the String.cpp
@@ -0,0 +1,16 @@
+/*Problem statement
+You are given a string 'STR'. The string contains [a-z] [A-Z] [0-9] [special characters]. You have to find the reverse of the string.
+
+For example:
+
+ If the given string is: STR = "abcde". You have to print the string "edcba".
+follow up:
+Try to solve the problem in O(1) space complexity. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseString(string s) {
+    reverse(s.begin(), s.end());
+    return s;
+}
